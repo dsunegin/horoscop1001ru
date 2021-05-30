40ef5ad8dacddbdd80b8f34b3a8ed06f1664dabc
Denis Sunegin <dsunegin@gmail.com>
2021-05-30 17:12:59 +0100

    Initial commit

--- pm2-load-horoscop1001ru.sh
+++ pm2-load-horoscop1001ru.sh
@@ -2,12 +2,12 @@
 
 npm run compile
 
-
 pm2 status | grep load-horoscop1001ru && pm2 delete load-horoscop1001ru
 pm2 status | grep load-horoscop1001ru-refact && pm2 delete load-horoscop1001ru-refact
+
 # Load Start each 6 hours
-CRON="0 */6 * * *" pm2 start npm --name load-horoscop1001ru  -- run start
+CRON="0 */6 * * *" pm2 start --name load-horoscop1001ru build/src/index.js
 # Refact Start at 20:00
-CRON="00 20 * * *" pm2 start npm --name load-horoscop1001ru-refact  build/src/refact.js
+CRON="00 20 * * *" pm2 start --name load-horoscop1001ru-refact  build/src/refact.js
 
 pm2 save
